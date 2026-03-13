dc7eb96196d5d18b3283389f65008ce0fc848190
adnan.alnajjar <adnan.alnajjar@ceteq.de>
2026-03-13 10:38:40 +0100

    css files & html optimization

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Host, Input, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css', encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();

a05b1abdac575d26c5220820bfa6fbc502ea65ee
adnan.alnajjar <adnan.alnajjar@ceteq.de>
2026-03-16 11:43:04 +0100

    Tickets fixed

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,13 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild, ViewChildren } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, ButtonComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
